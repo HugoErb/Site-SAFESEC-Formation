fe662c64eb5ead70c071b6cc47ffbae0a4dd4e49
HugoErb <erb.hugo@yahoo.fr>
2024-04-30 13:01:49 +0200

    fix ts page training form

--- frontend/src/app/training-form/training-form.component.ts
+++ frontend/src/app/training-form/training-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-training-form',
@@ -8,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrl: './training-form.component.scss'
 })
 export class TrainingFormComponent {
+  burgerMenuOpened: boolean = false;
 
+  @ViewChild('menuContainerRef') menuContainerRef!: ElementRef;
+  @ViewChild('menuBurger') menuBurger!: ElementRef;
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: MouseEvent) {
+    if (this.burgerMenuOpened && !this.menuBurger.nativeElement.contains(event.target)) {
+      this.burgerMenuOpened = false;
+    }
+  }
+
+  scrollToSection(sectionId: string): void {
+    if (this.burgerMenuOpened) {
+      this.burgerMenuOpened = !this.burgerMenuOpened;
+    }
+
+    setTimeout(() => {
+      const section = document.getElementById(sectionId);
+      if (section) {
+        const sectionTop = section.getBoundingClientRect().top + window.scrollY;
+        const headerHeight = 64;
+        const position = sectionTop - headerHeight;
+        window.scrollTo({ top: position, behavior: 'smooth' });
+      }
+    }, 50);
+  }
+
+  toggleBurgerMenu(event: MouseEvent): void {
+    event.stopPropagation();
+    this.burgerMenuOpened = !this.burgerMenuOpened;
+  }
+
+  sendMail() {
+  }
 }
