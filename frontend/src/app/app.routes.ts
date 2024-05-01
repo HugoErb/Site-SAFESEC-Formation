bb2cfd444d6750c1d4ac7fa8685b5fb6ed03662f
HugoErb <erb.hugo@yahoo.fr>
2024-05-01 14:54:03 +0200

    gestion de la route home + changement première personne

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,5 +4,6 @@ import { TrainingFormComponent } from './training-form/training-form.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'training-form', component: TrainingFormComponent }
 ];
