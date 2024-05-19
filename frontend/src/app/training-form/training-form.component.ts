72b763fef67cd72066ce0c6afe172517753237f6
HugoErb <erb.hugo@yahoo.fr>
2024-05-19 21:39:08 +0200

    optimisation de l'envoi de mail + envoi du message de reception + fix envoi de mail home + docstring

--- frontend/src/app/training-form/training-form.component.ts
+++ frontend/src/app/training-form/training-form.component.ts
@@ -9,12 +9,6 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCircleArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
-// Définition de l'interface pour la réponse de l'API de MailCheck.ai
-interface EmailValidityResponse {
-  disposable: boolean;
-  mx: boolean;
-}
-
 @Component({
   selector: 'app-training-form',
   standalone: true,
@@ -54,6 +48,15 @@ export class TrainingFormComponent implements OnInit {
     }
   }
 
+  /**
+  * Gère les clics à l'extérieur du menu burger pour fermer le menu.
+  * 
+  * Cette méthode est déclenchée par un écouteur d'événements qui surveille tous les clics dans le document.
+  * Si le menu burger est ouvert et que le clic n'est pas dans le menu burger,
+  * alors le menu sera fermé. Ceci est vérifié en utilisant la méthode `contains` sur l'élément natif du menu burger.
+  * 
+  * @param event L'objet MouseEvent associé au clic du document.
+  */
   @ViewChild('menuContainerRef') menuContainerRef!: ElementRef;
   @ViewChild('menuBurger') menuBurger!: ElementRef;
   @HostListener('document:click', ['$event'])
@@ -63,10 +66,21 @@ export class TrainingFormComponent implements OnInit {
     }
   }
 
+  /**
+  * Navigue vers la route d'accueil et inclut une section de redirection dans les paramètres de la route.
+  * 
+  * @param {string} section - La section de la route d'accueil vers laquelle naviguer. Cette valeur sera incluse en tant que paramètre dans la navigation.
+  */
   navigateToHome(section: string) {
     this.router.navigate(['home', { redirectionSection: section }]);
   }
 
+  /**
+  * Sert à ouvrir ou fermer le menu burger en inversant l'état actuel du menu. 
+  * Elle arrête également la propagation de l'événement de clic pour éviter des interactions indésirables avec d'autres éléments de l'interface utilisateur.
+  * 
+  * @param {MouseEvent} event - L'événement de clic qui a déclenché l'appel de la méthode. Utilisé pour arrêter la propagation de l'événement.
+  */
   toggleBurgerMenu(event: MouseEvent): void {
     event.stopPropagation();
     this.burgerMenuOpened = !this.burgerMenuOpened;
--- server.js
+++ server.js
@@ -33,25 +33,54 @@ const transporter = nodemailer.createTransport({
   }
 });
 
-app.post('/sendmail', (req, res) => {
+app.post('/sendmail', async (req, res) => {
+  // Extraire les valeurs en fonction de l'ordre attendu
+  const [name, email, phoneNumber, message] = Object.values(req.body);
 
-  const { name, email, phoneNumber, message } = req.body;
+  // Vérification que les valeurs nécessaires sont présentes
+  if (!name || !email || !phoneNumber || !message) {
+    return res.status(400).json({ error: 'Certaines valeurs nécessaires sont manquantes.' });
+  }
 
-  // Préparer l'email
-  const mailOptions = {
-    from: email,
-    to: 'safesecformation@gmail.com',
-    subject: `Nouveau message de ${name}`,
-    text: `Nom : ${name}\nEmail : ${email}\nNuméro de tél : ${phoneNumber}\n\nMessage : ${message}`
-  };
-
-  // Envoyer l'email
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      return res.status(500).json({ error: error.message });
-    }
-    res.status(200).json({ message: 'Email envoyé avec succès !', info: info });
+  // Fonction pour préparer les options d'envoi d'email
+  const prepareMailOptions = (from, to, subject, text) => ({
+    from,
+    to,
+    subject,
+    text,
   });
+
+  // Préparer les emails
+  const mailOptionsSSF = prepareMailOptions(
+    email,
+    'safesecformation@gmail.com',
+    `Nouveau message de ${name}`,
+    `Nom : ${name}\nEmail : ${email}\nNuméro de tél : ${phoneNumber}\n\nMessage : ${message}`
+  );
+
+  const mailOptionsConfirmation = prepareMailOptions(
+    'safesecformation@gmail.com',
+    email,
+    `SAFESEC Formation - Réception de votre message`,
+    `Bonjour !\n\nNous avons bien reçu votre message. Nous allons l'examiner et nous y répondrons dans les plus brefs délais.\nEn attendant, vous pouvez visiter notre site internet ou mon Linkedin. Merci pour votre confiance !\n\nSAFESEC Formation`
+  );
+
+  try {
+    // Envoyer le premier email
+    const infoSSF = await transporter.sendMail(mailOptionsSSF);
+
+    // Si le premier email est envoyé avec succès, envoyer le deuxième email
+    const infoConfirmation = await transporter.sendMail(mailOptionsConfirmation);
+
+    res.status(200).json({
+      message: 'Emails envoyés avec succès !',
+      infoSSF: infoSSF,
+      infoConfirmation: infoConfirmation,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
