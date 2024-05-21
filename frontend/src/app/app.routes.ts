cf36a03c6809f58a2a3c716ce2fb4bb43243f621
HugoErb <erb.hugo@yahoo.fr>
2024-05-21 21:26:00 +0200

    ajout de la route legal-information + configuration du component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { TrainingFormComponent } from './training-form/training-form.component';
+import { LegalInfosComponent } from './legal-infos/legal-infos.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full'},
-  { path: 'training-form', component: TrainingFormComponent }
+  { path: 'training-form', component: TrainingFormComponent },
+  { path: 'legal-information', component: LegalInfosComponent }
 ];
--- frontend/src/app/legal-infos/legal-infos.component.ts
+++ frontend/src/app/legal-infos/legal-infos.component.ts
@@ -1,16 +1,59 @@
-import { Component, ElementRef, HostListener, ViewChild, OnInit, ViewChildren, QueryList } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, ElementRef, HostListener, ViewChild} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { CommonService } from '../common.service';
+
+// Font Awesome Icons
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-legal-infos',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './legal-infos.component.html'
 })
-export class LegalInfosComponent implements OnInit {
+export class LegalInfosComponent {
   constructor(private router: Router, private activatedRoute: ActivatedRoute, private commonService: CommonService) {
+  }
+  icons = {faBars};
+  burgerMenuOpened: boolean = false;
+
+  /**
+  * Gère les clics à l'extérieur du menu burger pour fermer le menu.
+  * 
+  * Cette méthode est déclenchée par un écouteur d'événements qui surveille tous les clics dans le document.
+  * Si le menu burger est ouvert et que le clic n'est pas dans le menu burger,
+  * alors le menu sera fermé. Ceci est vérifié en utilisant la méthode `contains` sur l'élément natif du menu burger.
+  * 
+  * @param event L'objet MouseEvent associé au clic du document.
+  */
+  @ViewChild('menuContainerRef') menuContainerRef!: ElementRef;
+  @ViewChild('menuBurger') menuBurger!: ElementRef;
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: MouseEvent) {
+    if (this.burgerMenuOpened && !this.menuBurger.nativeElement.contains(event.target)) {
+      this.burgerMenuOpened = false;
+    }
+  }
+
+  /**
+  * Navigue vers la route d'accueil et inclut une section de redirection dans les paramètres de la route.
+  * 
+  * @param {string} section - La section de la route d'accueil vers laquelle naviguer. Cette valeur sera incluse en tant que paramètre dans la navigation.
+  */
+  navigateToHome(section: string) {
+    this.router.navigate(['home', { redirectionSection: section }]);
+  }
+
+  /**
+  * Sert à ouvrir ou fermer le menu burger en inversant l'état actuel du menu. 
+  * Elle arrête également la propagation de l'événement de clic pour éviter des interactions indésirables avec d'autres éléments de l'interface utilisateur.
+  * 
+  * @param {MouseEvent} event - L'événement de clic qui a déclenché l'appel de la méthode. Utilisé pour arrêter la propagation de l'événement.
+  */
+  toggleBurgerMenu(event: MouseEvent): void {
+    event.stopPropagation();
+    this.burgerMenuOpened = !this.burgerMenuOpened;
   }
 }
