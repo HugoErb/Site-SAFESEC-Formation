2291717d90fcc47e96d013f8a6929d41eab0a57d
HugoErb <erb.hugo@yahoo.fr>
2024-09-19 16:09:20 +0000

    rollback test OK + fix redeploiement.sh

--- redeploiement.sh
+++ redeploiement.sh
@@ -2,14 +2,14 @@
 
 # Définir les chemins nécessaires
 dossierRacine=$(pwd)  # Le dossier racine du site est le dossier courant
-dossierDistFrontend="$dossierRacine/frontend/dist"
-dossierDistRacine="$dossierRacine/dist"
+dossierDistRacine="$dossierRacine/dist"  # Dossier de destination à la racine
 
-# Supprimer le dossier dist à la racine
-rm -rf $dossierDistRacine
-
-# Mettre à jour le dépôt avec git
+# Mettre à jour le dépôt avec git en premier
 git pull origin
+
+# Supprimer les dossiers dist à la racine et dans frontend
+rm -rf $dossierDistRacine
+rm -rf $dossierRacine/frontend/dist  # On supprime tout le dossier dist dans frontend
 
 # Se déplacer dans le dossier frontend pour le build
 cd frontend/
@@ -23,8 +23,16 @@ wait
 # Revenir au dossier racine
 cd $dossierRacine
 
-# Copier le dossier frontend/dist vers la racine et le renommer dist
-cp -r $dossierDistFrontend $dossierDistRacine
+# Trouver le dossier dans frontend/dist
+dossierDistFrontend=$(find "$dossierRacine/frontend/dist" -mindepth 1 -maxdepth 1 -type d)
+
+# Copier le dossier trouvé dans frontend/dist vers la racine et le renommer dist
+if [ -d "$dossierDistFrontend" ]; then
+    cp -r $dossierDistFrontend $dossierDistRacine
+else
+    echo "Erreur : Aucun dossier trouvé dans frontend/dist"
+    exit 1
+fi
 
 # Redémarrer l'application avec PM2 (id 0 dans cet exemple)
 pm2 restart 0
